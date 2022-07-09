9529ed1d639e7bf1cea8ac67945dc3b3320bcd32
jose <jgradosc@gmail.com>
2022-07-08 23:37:15 -0500

    Dejé de usar vectores y usé una cola con la librería <queue> y dejó ahora si aguanta el proceso de manera infinita

--- monitorFinal.cpp
+++ monitorFinal.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-#include<thread>
-#include<mutex>
-#include <vector>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <queue>
 
 using namespace std;
 
@@ -9,29 +9,34 @@ using namespace std;
 
 #define NUM_HILOS 10
 
-vector<char> cola;
-const char abededario[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-int contador = 0;
+queue<char> cola;
+const char abededario[27] ={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+int cont=0;
+
 mutex flag;
 
 class Monitor {
 	public:
 	void producir(int productor){
 		flag.lock();	// LOCK
+    
 		int j = rand() % 26;
-		char producido=abededario[j];
-		cola.push_back(producido);
-		cout<<"Productor número "<<productor<<" ha producido: "<<producido<<"\n";
+		char letra = abededario[j];
+		cola.push(letra);
+		cout<<"Productor número "<<productor<<" ha producido: "<<letra<<"\n";
+    
 		flag.unlock();	// UNLOCK		
 	}
 	
 	void consumir(int consumidor){
 	  flag.lock();	// LOCK
-		int aleatorio;
-		aleatorio = rand() % 26;
-		char consumido=abededario[aleatorio];
-		cola.pop_back();
-		cout<<"Consumidor ->"<<consumidor<<"<- consumió: "<<consumido<<endl;
+    
+		int j = rand() % 26;
+		char letra = abededario[j];
+		cola.pop();
+		cout<<"Consumidor número "<<consumidor<<" ha consumido: "<<letra<<"\n";
+    
 		flag.unlock();	// UNLOCK		
 	}
 };
@@ -42,15 +47,15 @@ class Productor {
 		thread t;
 		int id;
 		void run_thread(){			
-			for (int i=1; true; i++) {
+			for (int i = 1; true; i++) {
 			    monitor->producir(id);		//	RUN
 			}
 		}
 			
 	public:
-		Productor(Monitor* mon, int idd){
-			id = idd;
-			monitor = mon;
+		Productor(Monitor* monitor1, int identificadorP){
+			id = identificadorP;
+			monitor = monitor1;
 			t = thread(&Productor::run_thread, this);
 		}
 		void join_thread(){
@@ -65,15 +70,15 @@ class Consumidor {
 		thread t;
 		int id;
 		void run_thread(){			
-			for (int i=1; true; i++) {
+			for (int i = 1; true; i++) {
 			    monitor->consumir(id);		//	RUN
 			}
 		}
 			
 	public:
-		Consumidor(Monitor* mon, int idd){
-			id = idd;
-			monitor = mon;
+		Consumidor(Monitor* monitor2, int identificadorC){
+			id = identificadorC;
+			monitor = monitor2;
 			t = thread(&Consumidor::run_thread, this);
 		}
 		void join_thread(){
@@ -90,14 +95,14 @@ int main() {
 	Monitor* monitor;
 	
 	int i;
-	for(i=0; i<NUM_HILOS; i++) {
+	for(i=0; i < NUM_HILOS; i++) {
 		productor[i] = new Productor(monitor, i);
 		consumidor[i] = new Consumidor(monitor, i);
 	}
 	
-	for(i=0; i<NUM_HILOS; i++) {
+	for(i=0; i < NUM_HILOS; i++) {
 		productor[i]->join_thread();	//	START
-	    consumidor[i]->join_thread();	//	START
+	  consumidor[i]->join_thread();	//	START
 	}
 
 	return 0;
